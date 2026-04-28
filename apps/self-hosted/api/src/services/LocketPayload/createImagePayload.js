bd4ebae3b1ff214b5636228603422b7ff4fd5f5b
lvquyen15506 <protv2006@gmail.com>
2026-04-28 23:34:24 +0700

    fix: exhaustive header and overlay updates for caption compatibility based on successful fetch example

--- apps/self-hosted/api/src/services/LocketPayload/createImagePayload.js
+++ apps/self-hosted/api/src/services/LocketPayload/createImagePayload.js
@@ -36,7 +36,7 @@ exports.imagePostPayloadDecorative = ({ imageUrl, optionsData }) => {
       },
     },
     alt_text: caption,
-    overlay_id: `caption:${overlay_id}`,
+    overlay_id: overlay_id === "standard" ? "caption" : `caption:${overlay_id}`,
     overlay_type: "caption",
   });
 
